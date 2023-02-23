a5c51a9b2888bc806fe79a48e439fd3703ce4112
martin <martinrtownley@gmail.com>
2023-02-23 11:50:47 +0000

    add checking for specific review id, and specific comments array length

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -129,11 +129,12 @@ describe("app", () => {
           .expect(200)
           .then(({ body }) => {
             expect(body.comments).toBeInstanceOf(Array);
+            expect(body.comments.length).toBe(3);
             body.comments.forEach((comment) => {
               expect(comment).toMatchObject({
                 comment_id: expect.any(Number),
                 votes: expect.any(Number),
-                review_id: expect.any(Number),
+                review_id: 2,
                 created_at: expect.any(String),
                 body: expect.any(String),
                 author: expect.any(String),
